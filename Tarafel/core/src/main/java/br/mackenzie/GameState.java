7f6c70152a7e7d28f83646111c06afe37f454ae6
carmoso <eduardo.carmo@mackenzista.com.br>
2026-05-01 12:57:20 -0300

    Implementa GameState

--- Tarafel/core/src/main/java/br/mackenzie/GameState.java
+++ Tarafel/core/src/main/java/br/mackenzie/GameState.java
@@ -1,18 +1,66 @@
 package br.mackenzie;
 
 public class GameState {
-    public boolean paused = false;
-    public boolean gameOver = false;
-    public int score = 0;
-    public int lives = 3;
-    public int level = 1;
+    private boolean paused = false;
+    private boolean gameOver = false;
+    private int score = 0;
+    private int lives = 3;
+    private int level = 1;
+    private int levelDefenses;
 
+    public static final int life_max = 3;
+    public static final int defensesAdvance = 5;
 
+    private GameTransitionListener transitionListener;
+
+    public interface GameTransitionListener {
+        void onGameOver();
+
+        void onNextLevel(int newLevel);
+    }
+
+    public GameState() {
+        reset();
+    }
+
+    public void setTransitinListener(GameTransitionListener listener) {
+        this.transitionListener = listener;
+    }
+
+    public void defense() {
+        if (gameOver || paused)
+            return;
+
+        addScore(1);
+        levelDefenses++;
+        checkLevelProgress();
+    }
+
+    public void goal() {
+        if (gameOver || paused)
+            return;
+
+        removeLife(1);
+        checkGameOver();
+    }
+
+    public void checkLevelProgress() {
+        if (levelDefenses >= defensesAdvance) {
+            level++;
+            levelDefenses = 0;
+
+            if (transitionListener != null) {
+                transitionListener.onNextLevel(level);
+            }
+        }
+
+    }
 
     public void reset() {
         paused = false;
+        this.gameOver = false;
         score = 0;
-        lives = 3;
+        lives = life_max;
         level = 1;
         gameOver = false;
     }
@@ -41,6 +89,36 @@ public class GameState {
     public void checkGameOver() {
         if (lives <= 0) {
             gameOver();
+
+            if (transitionListener != null) {
+                transitionListener.onGameOver();
+            }
         }
     }
+
+    private void addScore(int points) {
+        this.score += points;
+    }
+
+    private void removeLife(int amount) {
+        this.lives = Math.max(0, this.lives - amount);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getLevelDefenses() {
+        return levelDefenses;
+    }
+
+    
 }
